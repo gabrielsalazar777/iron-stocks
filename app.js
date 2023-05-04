0c130084c1e0da746c7982d1ffe3062fd6044a1e
Gabriel Salazar <gabriel.salazar777@gmail.com>
2023-05-04 12:41:35 -0400

    index and register page fully styled

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
+// hbs.registerPartials(__dirname + "");
 
 app.use(logger("dev"));
 app.use(express.json());
